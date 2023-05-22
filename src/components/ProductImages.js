b02b98e0feb548d94edfa6ffdf37a7507ab9ea56
adama diouf <adamadiouf2017@gmail.com>
2023-05-21 19:59:01 -0500

    update

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,44 +1,4 @@
 
-// import React, { useState, useEffect } from 'react'
-
-// const ProductImages = ({ images = [] }) => {
-//   const [main, setMain] = useState('')
-
-//   useEffect(() => {
-//     if (images.length > 0) {
-//       setMain(images[0])
-//     }
-//   }, [images])
-
-//   const handleImageClick = (image) => {
-//     setMain(image)
-//   }
-
-//   return (
-//     <div className='product-images'>
-//       {main && (
-//         <img
-//           src={main}
-//           alt='main'
-//           className={`main ${images.length > 0 ? 'active' : ''}`}
-//         />
-//       )}
-//       <div className='gallery'>
-//         {images.map((image, index) => (
-//           <img
-//             src={image}
-//             alt={`Product Image ${index}`}
-//             key={index}
-//             onClick={() => handleImageClick(image)}
-//             className={`${image === main ? 'active' : ''} slider-item`}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default ProductImages
 
 import React, { useState, useEffect } from 'react'
 
